0b7fb1f47a3c14da466eed9cfa4a581f0e37ae52
Bill Blight <watchersphone@gmail.com>
2017-08-31 14:22:07 -0400

    More prebuild, .NET 4.5 upddates

--- Prebuild/runprebuild.sh
+++ Prebuild/runprebuild.sh
@@ -1,3 +1,32 @@
 #!/bin/sh
-#exec mono [PREFIX]/prebuild.exe "$@"
-exec mono Prebuild.exe /target VS2010 
+
+case "$1" in
+
+  'clean')
+
+    mono bin/Prebuild.exe /clean
+
+  ;;
+
+
+  'autoclean')
+
+    echo y|mono bin/Prebuild.exe /clean
+
+  ;;
+
+
+  'vs2010')
+  
+    mono bin/Prebuild.exe /target vs2015
+  
+  ;;
+
+  *)
+
+    mono bin/Prebuild.exe /target nant
+    mono bin/Prebuild.exe /target vs2015
+
+  ;;
+
+esac
