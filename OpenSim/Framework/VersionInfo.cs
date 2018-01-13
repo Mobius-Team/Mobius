e5381bd22eb17cfbf7646cd4a8b72a7de9c4ce90
Bill Blight <watchersphone@gmail.com>
2018-01-13 16:05:34 -0500

    Switch version to Merovingian and update rev to20260c

--- OpenSim/Framework/VersionInfo.cs
+++ OpenSim/Framework/VersionInfo.cs
@@ -32,14 +32,14 @@ namespace OpenSim
         public const string VersionNumber = "0.9.1.0";
         public const string AssemblyVersionNumber = "0.9.1.*";
 
-        public const Flavour VERSION_FLAVOUR = Flavour.osCore2_Oberon_d031e3;
+        public const Flavour VERSION_FLAVOUR = Flavour.osCore2_Merovingian_20260c;
 
 
         public enum Flavour
         {
             Unknown,
             osCore2_Dev,
-	    osCore2_Oberon_d031e3,
+	    osCore2_Merovingian_20260c,
             osCore2_RC1,
             osCore2_RC2,
             osCore2_RC3,
