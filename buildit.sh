ee5cb651b2f5a4062f5030704abe9807f2addb6b
Bill Blight <watchersphone@gmail.com>
2018-01-19 04:51:01 -0500

    Added buildit.sh to build release

---
+++ buildit.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+nant /clean
+echo "Scrubbing Directories"
+find . -name "*.csproj" -type f -print0 | xargs -0 /bin/rm -f
+find . -name "*.csproj.user" -type f -print0 | xargs -0 /bin/rm -f
+find . -name "*.build" -type f -print0 | xargs -0 /bin/rm -f
+find . -name "*Temporary*" -type f -print0 | xargs -0 /bin/rm -f
+find . -name "*.cache" -type f -print0 | xargs -0 /bin/rm -f
+find . -name "*.rej" -type f -print0 | xargs -0 /bin/rm -f
+find . -name "*.orig" -type f -print0 | xargs -0 /bin/rm -f
+find . -name "*.pdb" -type f -print0 | xargs -0 /bin/rm -f
+find . -name "*.mdb" -type f -print0 | xargs -0 /bin/rm -f
+echo "Running Prebuild"
+./runprebuild.sh
+echo "Building Release, MS Debug Info sucks on linux"
+msbuild /p:Configuration=Release;AutoGenerateBindingRedirects=true
+echo "Build Done, check for errors"
