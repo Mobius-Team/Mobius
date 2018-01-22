2bcf0b9254ef9e8724fa1bc69eebf280cae87163
Bill Blight <watchersphone@gmail.com>
2018-01-21 19:03:21 -0500

    fix typo

--- MuteWebRoot/databaseinfo.php
+++ MuteWebRoot/databaseinfo.php
@@ -1,7 +1,7 @@
 <?php
 // By default this should be the same database as your opensimulator uses
-$DB_HOST = "grid.opensim.life";
-$DB_USER = "osdbadmin";
-$DB_PASSWORD = "Thx001138";
-$DB_NAME = "opensimlife";
+$DB_HOST = "localhost";
+$DB_USER = "root";
+$DB_PASSWORD = "password";
+$DB_NAME = "opensim";
 ?>
