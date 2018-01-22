13226c29c5153777c501a79d07abb0dbe94b4cbb
Bill Blight <watchersphone@gmail.com>
2018-01-22 18:56:32 -0500

    test fix 2 data snapshot throttle revert

--- OpenSim/Region/OptionalModules/DataSnapshot/DataSnapshotManager.cs
+++ OpenSim/Region/OptionalModules/DataSnapshot/DataSnapshotManager.cs
@@ -32,6 +32,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Reflection;
+using System.Threading;
 using System.Text;
 using System.Xml;
 using log4net;
@@ -64,6 +65,7 @@ namespace OpenSim.Region.DataSnapshot
         //Various internal objects
         private static readonly ILog m_log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
         internal object m_syncInit = new object();
+        private object m_serializeGen = new object();
 
         //DataServices and networking
         private string m_dataServices = "noservices";
@@ -148,11 +150,8 @@ namespace OpenSim.Region.DataSnapshot
                         m_enabled = false;
                         return;
                     }
-
                 }
-
             }
-
         }
 
         public void AddRegion(Scene scene)
@@ -162,45 +161,7 @@ namespace OpenSim.Region.DataSnapshot
 
             m_log.DebugFormat("[DATASNAPSHOT]: Module added to Scene {0}.", scene.RegionInfo.RegionName);
 
-            if (!m_servicesNotified)
-            {
-                m_hostname = scene.RegionInfo.ExternalHostName;
-                m_snapStore = new SnapshotStore(m_snapsDir, m_gridinfo, m_listener_port, m_hostname);
-
-                //Hand it the first scene, assuming that all scenes have the same BaseHTTPServer
-                new DataRequestHandler(scene, this);
-
-                if (m_dataServices != "" && m_dataServices != "noservices")
-                    NotifyDataServices(m_dataServices, "online");
-
-                m_servicesNotified = true;
-            }
-
             m_scenes.Add(scene);
-            m_snapStore.AddScene(scene);
-
-            Assembly currentasm = Assembly.GetExecutingAssembly();
-
-            foreach (Type pluginType in currentasm.GetTypes())
-            {
-                if (pluginType.IsPublic)
-                {
-                    if (!pluginType.IsAbstract)
-                    {
-                        if (pluginType.GetInterface("IDataSnapshotProvider") != null)
-                        {
-                            IDataSnapshotProvider module = (IDataSnapshotProvider)Activator.CreateInstance(pluginType);
-                            module.Initialize(scene, this);
-                            module.OnStale += MarkDataStale;
-
-                            m_dataproviders.Add(module);
-                            m_snapStore.AddProvider(module);
-
-                            m_log.Debug("[DATASNAPSHOT]: Added new data provider type: " + pluginType.Name);
-                        }
-                    }
-                }
-            }
 
         }
 
@@ -244,8 +205,47 @@ namespace OpenSim.Region.DataSnapshot
             if (!m_enabled)
                 return;
 
-            m_log.DebugFormat("[DATASNAPSHOT]: Marking scene {0} as stale.", scene.RegionInfo.RegionName);
-            m_snapStore.ForceSceneStale(scene);
+            if (m_snapStore == null)
+            {
+                m_hostname = scene.RegionInfo.ExternalHostName;
+                m_snapStore = new SnapshotStore(m_snapsDir, m_gridinfo, m_listener_port, m_hostname);
+
+                //Hand it the first scene, assuming that all scenes have the same BaseHTTPServer
+                new DataRequestHandler(scene, this);
+            }
+
+            m_snapStore.AddScene(scene);
+
+            Assembly currentasm = Assembly.GetExecutingAssembly();
+
+            foreach (Type pluginType in currentasm.GetTypes())
+            {
+                if (pluginType.IsPublic)
+                {
+                    if (!pluginType.IsAbstract)
+                    {
+                        if (pluginType.GetInterface("IDataSnapshotProvider") != null)
+                        {
+                            IDataSnapshotProvider module = (IDataSnapshotProvider)Activator.CreateInstance(pluginType);
+                            module.Initialize(scene, this);
+                            module.OnStale += MarkDataStale;
+
+                            m_dataproviders.Add(module);
+                            m_snapStore.AddProvider(module);
+
+                            m_log.Debug("[DATASNAPSHOT]: Added new data provider type: " + pluginType.Name);
+                        }
+                    }
+                }
+            }
+
+            if (!m_servicesNotified)
+            {
+                if (m_dataServices != "" && m_dataServices != "noservices")
+                    NotifyDataServices(m_dataServices, "online");
+
+                m_servicesNotified = true;
+            }
         }
 
         public void Close()
@@ -256,7 +256,6 @@ namespace OpenSim.Region.DataSnapshot
             if (m_enabled && m_dataServices != "" && m_dataServices != "noservices")
                 NotifyDataServices(m_dataServices, "offline");
         }
-
 
         public string Name
         {
@@ -319,8 +318,14 @@ namespace OpenSim.Region.DataSnapshot
         /**
          * Reply to the http request
          */
+
         public XmlDocument GetSnapshot(string regionName)
         {
+            if(!Monitor.TryEnter(m_serializeGen,30000))
+            {
+                return null;
+            }
+
             CheckStale();
 
             XmlDocument requestedSnap = new XmlDocument();
@@ -360,9 +365,13 @@ namespace OpenSim.Region.DataSnapshot
                 m_log.Warn("[DATASNAPSHOT]: Caught unknown exception while trying to load snapshot: " + e.StackTrace);
                 requestedSnap = GetErrorMessage(regionName, e);
             }
-
+            finally
+            {
+                Monitor.Exit(m_serializeGen);
+            }
 
             return requestedSnap;
+
         }
 
         private XmlDocument GetErrorMessage(string regionName, Exception e)
