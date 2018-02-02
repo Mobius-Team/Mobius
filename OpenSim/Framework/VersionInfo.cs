dc914838e5516756728981e57f9db4569d72c5a0
Bill Blight <watchersphone@gmail.com>
2018-02-02 13:31:47 -0500

    update rev Lysander_aa076f

--- OpenSim/Framework/VersionInfo.cs
+++ OpenSim/Framework/VersionInfo.cs
@@ -32,14 +32,14 @@ namespace OpenSim
         public const string VersionNumber = "0.9.1.1";
         public const string AssemblyVersionNumber = "0.9.1.1";
 
-        public const Flavour VERSION_FLAVOUR = Flavour.osC2_Puck_fa5efb;
+        public const Flavour VERSION_FLAVOUR = Flavour.osC2_Lysander_aa076f;
 
 
         public enum Flavour
         {
             Unknown,
             osC2_Dev,
-	    osC2_Puck_fa5efb,
+	    osC2_Lysander_aa076f,
             osC2_RC1,
             osC2_RC2,
             osC2_RC3,
