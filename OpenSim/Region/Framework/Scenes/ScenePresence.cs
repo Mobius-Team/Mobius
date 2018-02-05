c4b84ab5d1f50af9f37049cb55ced5a73273cc15
Bill Blight <watchersphone@gmail.com>
2018-02-05 02:02:04 -0500

    Increase wait time before TP fail to 30 seonds

--- OpenSim/Region/Framework/Scenes/ScenePresence.cs
+++ OpenSim/Region/Framework/Scenes/ScenePresence.cs
@@ -1977,7 +1977,7 @@ namespace OpenSim.Region.Framework.Scenes
 
             try
             {
-                if(m_updateAgentReceivedAfterTransferEvent.WaitOne(20000))
+                if(m_updateAgentReceivedAfterTransferEvent.WaitOne(30000))
                 {
                     UUID originID = UUID.Zero;
 
