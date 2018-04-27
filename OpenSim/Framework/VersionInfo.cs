a9e5284a83163af5657e675659092770de5a0782
Bill Blight <grid@opensim.life>
2018-04-27 08:07:28 -0400

    update rev osC2_Ragnarok_34e533

--- OpenSim/Framework/VersionInfo.cs
+++ OpenSim/Framework/VersionInfo.cs
@@ -32,14 +32,14 @@ namespace OpenSim
         public const string VersionNumber = "0.9.1.1";
         public const string AssemblyVersionNumber = "0.9.1.1";
 
-        public const Flavour VERSION_FLAVOUR = Flavour.osC2_Ragnarok_5b731d;
+        public const Flavour VERSION_FLAVOUR = Flavour.osC2_Ragnarok_34e533;
 
 
         public enum Flavour
         {
             Unknown,
             osC2_Dev,
-	    osC2_Ragnarok_5b731d,
+	    osC2_Ragnarok_34e533,
             osC2_RC1,
             osC2_RC2,
             osC2_RC3,
