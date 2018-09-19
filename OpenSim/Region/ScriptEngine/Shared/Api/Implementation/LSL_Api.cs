22626b59c489b4d191859f3c1a708adc3f2a05ca
Bill Blight <grid@opensim.life>
2018-09-18 19:56:01 -0700

    Let's get a clean revert

--- OpenSim/Region/ScriptEngine/Shared/Api/Implementation/LSL_Api.cs
+++ OpenSim/Region/ScriptEngine/Shared/Api/Implementation/LSL_Api.cs
@@ -6847,66 +6847,30 @@ namespace OpenSim.Region.ScriptEngine.Shared.Api
             m_host.SoundRadius = radius;
         }
 
-        public LSL_Key llName2Key(string name)
+        public LSL_String llKey2Name(string id)
         {
             m_host.AddScriptLPS(1);
-
-            if (!name.Contains(" "))
+            UUID key = new UUID();
+            if (UUID.TryParse(id,out key))
             {
-                if (!name.Contains("@"))
+                ScenePresence presence = World.GetScenePresence(key);
+
+                if (presence != null)
                 {
-                    if (name.Contains("."))
-                    {
-                        if (!name.EndsWith("."))
-                        {
-                            string[] parts = name.Split(new char[] { '.' });
-                            name = parts[0] + " " + parts[1];
-                        }
-                        else
-                            name = name + " Resident";
-                    }
-                    else
-                        name = name + " Resident";
+                    return presence.ControllingClient.Name;
+                    //return presence.Name;
                 }
-                else
+
+                if (World.GetSceneObjectPart(key) != null)
                 {
-                    if (!name.EndsWith("@"))
-                    {
-                        // First I assume, that on the right side of last ocurrence "@", there is not serverURI string
-                        string testname = name + " Resident";
-                        ScenePresence test = null;
-                        if (World.TryGetAvatarByName(testname, out test))
-                        {
-                            if (!test.IsNPC)
-                                return test.UUID.ToString();
-
-                            return UUID.Zero.ToString();
-                        }
-
-                        // If first assumption is wrong, then I assume, that there is serverURI string after last ocurrence of "@"
-                        string[] split = name.Split('@');
-                        string firstPart = string.Join("@", split.Take(split.Length - 1));
-                        string[] dots = firstPart.Split('.');
-                        firstPart = string.Join(".", dots.Take(dots.Length - 1));
-                        string lastPart = split.Last();
-                        name = firstPart + " @" + lastPart;
-                    }
-                    else
-                        name = name + " Resident";
+                    return World.GetSceneObjectPart(key).Name;
                 }
             }
-
-            ScenePresence avatar = null;
-            if (World.TryGetAvatarByName(name, out avatar))
-            {
-                if (!avatar.IsNPC)
-                    return avatar.UUID.ToString();
-            }
-
-            return UUID.Zero.ToString();
+            return String.Empty;
         }
 
- 
+
+
         public void llSetTextureAnim(int mode, int face, int sizex, int sizey, double start, double length, double rate)
         {
             m_host.AddScriptLPS(1);
