dd5ff157a6b2d27f59dc09ac2dcfdc0ddb586248
Bill Blight <grid@opensim.life>
2018-09-28 15:02:42 -0700

    Change osSetOwnerSpeed and it's limit to try and make YEngine happy

--- OpenSim/Region/ScriptEngine/Shared/Api/Implementation/OSSL_Api.cs
+++ OpenSim/Region/ScriptEngine/Shared/Api/Implementation/OSSL_Api.cs
@@ -3708,8 +3708,9 @@ public void osMakeScript(string scriptName, LSL_Types.list contents)
         public void osSetOwnerSpeed(LSL_Float SpeedModifier)
         {
             CheckThreatLevel(ThreatLevel.Moderate, "osSetOwnerSpeed");
-			float osSetOwnerSpeedLimit = m_osslconfig.GetFloat("osSetOwnerSpeedLimit");
-			if (osSetOwnerSpeedLimit < 4.0f || osSetOwnerSpeedLimit > 10.0f)osSetOwnerSpeedLimit = 4.0f; //This is to stop viewer crashes
+			LSL_Float osSetOwnerSpeedLimit = 2.0f;
+			osSetOwnerSpeedLimit = float.Parse(m_osslconfig.GetString("osSetOwnerSpeedLimit"));
+			if (osSetOwnerSpeedLimit < 2.0f || osSetOwnerSpeedLimit > 10.0f)osSetOwnerSpeedLimit = 2.0f; //This is to stop viewer crashes
             if(SpeedModifier > osSetOwnerSpeedLimit)SpeedModifier = osSetOwnerSpeedLimit;
             ScenePresence avatar = World.GetScenePresence(m_host.OwnerID);
 
