f5943ef0687c3b7c61669259371aa23ca4bdc2e3
Bill Blight <grid@opensim.life>
2019-02-24 16:36:06 -0800

    Per Ubit: also throttle llhttprequest by owner, options: PrimOwnerRequestsBurst = 5, PrimOwnerRequestsPerSec = 25; increase concurrency to 8

--- OpenSim/Region/ScriptEngine/Shared/Api/Implementation/LSL_Api.cs
+++ OpenSim/Region/ScriptEngine/Shared/Api/Implementation/LSL_Api.cs
@@ -14025,7 +14025,7 @@ namespace OpenSim.Region.ScriptEngine.Shared.Api
             if(httpScriptMod == null)
                 return "";
 
-            if(!httpScriptMod.CheckThrottle(m_host.LocalId))
+            if(!httpScriptMod.CheckThrottle(m_host.LocalId, m_host.OwnerID))
                 return UUID.Zero.ToString();
 
             List<string> param = new List<string>();
