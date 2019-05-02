d648776cb42ba125d3eff8436708ab44e88f2518
Bill Blight <grid@opensim.life>
2019-05-02 06:49:03 -0700

    update rev osC2_master_cf2ddf_064902_050219

--- OpenSim/Framework/VersionInfo.cs
+++ OpenSim/Framework/VersionInfo.cs
@@ -32,14 +32,14 @@ namespace OpenSim
         public const string VersionNumber = "0.9.1.1";
         public const string AssemblyVersionNumber = "0.9.1.1";
 
-        public const Flavour VERSION_FLAVOUR = Flavour.osC2_master_d599af_195418_043019;
+        public const Flavour VERSION_FLAVOUR = Flavour.osC2_master_cf2ddf_064902_050219;
 
 
         public enum Flavour
         {
             Unknown,
             osC2_Dev,
-	    osC2_master_d599af_195418_043019,
+	    osC2_master_cf2ddf_064902_050219,
             osC2_RC1,
             osC2_RC2,
             osC2_RC3,
