ad31ad99bd094b232845119ec8a345ffa9690870
Bill Blight <grid@opensim.life>
2019-10-16 10:32:24 -0700

    fix typo in last patch

--- OpenSim/Region/ScriptEngine/Shared/Api/Implementation/OSSL_Api.cs
+++ OpenSim/Region/ScriptEngine/Shared/Api/Implementation/OSSL_Api.cs
@@ -230,9 +230,10 @@ namespace OpenSim.Region.ScriptEngine.Shared.Api
                     break;
                 }
             try
-                    PSTTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Pacific Standard Time");            
 	    {
-                }
+	    	    PSTTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Pacific Standard Time");            
+	    {
+            
                 catch
                 {
                     PSTTimeZone = null;
