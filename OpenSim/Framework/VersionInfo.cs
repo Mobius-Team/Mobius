47cf85c5e81a1dc4f6ed8e625f949d2bc6c00fdd
Bill Blight <grid@opensim.life>
2019-11-09 16:07:38 -0800

    update rev osC2_Yeti_87fa82_160737_110919

--- OpenSim/Framework/VersionInfo.cs
+++ OpenSim/Framework/VersionInfo.cs
@@ -32,14 +32,14 @@ namespace OpenSim
         public const string VersionNumber = "0.9.1.1";
         public const string AssemblyVersionNumber = "0.9.1.1";
 
-        public const Flavour VERSION_FLAVOUR = Flavour.osC2_Yeti_7649a3_082506_110119;
+        public const Flavour VERSION_FLAVOUR = Flavour.osC2_Yeti_87fa82_160737_110919;
 
 
         public enum Flavour
         {
             Unknown,
             osC2_Dev,
-	    osC2_Yeti_7649a3_082506_110119,
+	    osC2_Yeti_87fa82_160737_110919,
             osC2_RC1,
             osC2_RC2,
             osC2_RC3,
